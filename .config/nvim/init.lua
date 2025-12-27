d335401e17d1db59c0bc88a35558dc27186c0d29
Derrick Reimer <derrickreimer@gmail.com>
2025-12-27 11:11:53 -0600

    Switch to treesitter master branch (main requires a big overhaul of config)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -941,10 +941,11 @@ require('lazy').setup({
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter', -- Sets main module to use for opts
+    branch = 'master',
+    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'elixir', 'heex', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
