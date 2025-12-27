eddeb5863490a91a8b36013391f4d87cf86cdb1b
Derrick Reimer <derrickreimer@gmail.com>
2025-12-27 09:15:41 -0600

    Add elixir lsp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -683,7 +683,7 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
-
+        elixirls = {},
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
