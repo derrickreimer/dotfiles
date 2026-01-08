b8205fcbabc09566d0d1a3e9489ca8ad4820aeaa
Derrick Reimer <derrickreimer@gmail.com>
2026-01-08 10:44:06 -0600

    Customize start screen a bit

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -866,7 +866,7 @@ require('lazy').setup({
             return nil
           else
             return {
-              timeout_ms = 500,
+              timeout_ms = 1000,
               lsp_format = 'fallback',
             }
           end
--- nvim/.config/nvim/lua/custom/plugins/nvim-startled.lua
+++ nvim/.config/nvim/lua/custom/plugins/nvim-startled.lua
@@ -3,5 +3,6 @@ return {
   lazy = false,
   opts = {
     -- All of your `setup(opts)` will go here
+    content = require 'custom.config.nvim-startled-content',
   },
 }
