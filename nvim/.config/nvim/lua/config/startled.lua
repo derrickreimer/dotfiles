0132d316fef96a548e31b1d9332c03f39979aca4
Derrick Reimer <derrickreimer@gmail.com>
2026-01-09 08:10:29 -0600

    Tidying arrangement

--- nvim/.config/nvim/lua/config/startled.lua
+++ nvim/.config/nvim/lua/config/startled.lua
@@ -10,39 +10,41 @@ local header = [[
 ]]
 
 return {
-  {
-    text = function()
-      local v = vim.version()
-      local version_text = v.major .. '.' .. v.minor .. '.' .. v.patch
-      return 'NVIM v' .. version_text
-    end,
-    hl = 'StartledSecondary',
-  },
-  {
-    type = 'spacer',
-  },
-  {
-    text = header,
-    hl = 'StartledPrimary',
-  },
-  {
-    type = 'spacer',
-  },
-  {
-    text = {
-      'type <StartledSecondary>:help</StartledSecondary><StartledMuted><Enter></StartledMuted> for help',
-      'type <StartledSecondary>:q</StartledSecondary><StartledMuted><Enter></StartledMuted> to exit',
+  content = {
+    {
+      text = function()
+        local v = vim.version()
+        local version_text = v.major .. '.' .. v.minor .. '.' .. v.patch
+        return 'NVIM v' .. version_text
+      end,
+      hl = 'StartledSecondary',
     },
-  },
-  {
-    type = 'spacer',
-  },
-  {
-    text = function()
-      local quotes = require 'startled.content.quotes'
-      return require('startled.utils').random(quotes)
-    end,
-    wrap = 60,
-    hl = 'StartledMuted',
+    {
+      type = 'spacer',
+    },
+    {
+      text = header,
+      hl = 'StartledPrimary',
+    },
+    {
+      type = 'spacer',
+    },
+    {
+      text = {
+        'type <StartledSecondary>:help</StartledSecondary><StartledMuted><Enter></StartledMuted> for help',
+        'type <StartledSecondary>:q</StartledSecondary><StartledMuted><Enter></StartledMuted> to exit',
+      },
+    },
+    {
+      type = 'spacer',
+    },
+    {
+      text = function()
+        local quotes = require 'startled.content.quotes'
+        return require('startled.utils').random(quotes)
+      end,
+      wrap = 60,
+      hl = 'StartledMuted',
+    },
   },
 }
--- nvim/.config/nvim/lua/plugins/startled.lua
+++ nvim/.config/nvim/lua/plugins/startled.lua
@@ -3,6 +3,6 @@ return {
   lazy = false,
   opts = {
     -- All of your `setup(opts)` will go here
-    content = require 'config.nvim-startled-content',
+    content = require('config.startled').content,
   },
 }
