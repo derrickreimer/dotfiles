b53d686af8f66c1b87ed750a991ddabc462b7776
Derrick Reimer <derrickreimer@gmail.com>
2026-01-18 11:41:08 -0600

    Use Tab to accept completion suggestions in blink.cmp

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -58,7 +58,7 @@ return {
       -- <c-k>: Toggle signature help
       --
       -- See :h blink-cmp-config-keymap for defining your own keymap
-      preset = 'default',
+      preset = 'super-tab',
 
       -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
       --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
