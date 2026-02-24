84584d36ad3bfb4a313ebe122df0f0ffb7aa83aa
Derrick Reimer <derrickreimer@gmail.com>
2026-02-24 08:37:08 -0600

    Add a few general aliases and functions

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -79,3 +79,33 @@ alias bb='brew bundle --file="$HOME/dotfiles/brew/Brewfile"'
 alias 7zap='7z a -p -mhe=on'
 alias 7zx='7z x'
 
+# ------------------------------------------------------------------------------
+# General aliases and functions
+# ------------------------------------------------------------------------------
+
+alias l='ls -lhA'
+alias c="clear"
+alias o="open ."
+alias rf="trash"
+alias sizes='du -sh -c *'
+alias cwd="pwd && pwd | pbcopy && echo 'Copied to clipboard 📁'"
+
+# Run any command from anywhere, without leaving current working directory.
+#
+# Usage: `in [target] [command]`
+# Target: directory (if available), else `z` argument
+# Example: `in sand art make:model -a SomeModel`
+function in() {(
+  if [ -d "$1" ]; then
+    cd $1
+  else
+    z $1
+  fi
+
+  eval ${@:2}
+)}
+
+# Use rlwrap to fix keyboard input in scripts/commands where it's borked
+function wrap() {
+  rlwrap --always-readline --no-children $1
+}
