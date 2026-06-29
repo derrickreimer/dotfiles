fb7c7547db9bd92ae1eac707b4f7ff6ba446eede
Derrick Reimer <derrickreimer@gmail.com>
2026-06-29 10:19:08 -0500

    Add eslint_d to mise, notify on conform format errors
    eslint_d was never installed, so conform's eslint-plugin-prettier
    formatting path silently no-op'd on save. Add it to the global mise
    config and flip conform's notify_on_error to surface future failures.

--- nvim/.config/nvim/lua/plugins/formatting.lua
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -48,7 +48,7 @@ return {
     end
 
     return {
-      notify_on_error = false,
+      notify_on_error = true,
       format_on_save = function(bufnr)
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
