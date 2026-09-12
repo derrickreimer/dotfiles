0b5fec686b375bc8c9abfad1c7506eef5e5c98cc
Derrick Reimer <derrickreimer@gmail.com>
2026-09-12 15:47:22 -0500

    Add nvim-colorizer and refresh the lazy.nvim lockfile

---
+++ nvim/.config/nvim/lua/plugins/colorizer.lua
@@ -0,0 +1,11 @@
+-- Colorizer: A color highlighter / previewer
+-- https://github.com/NvChad/nvim-colorizer.lua
+
+return {
+  'NvChad/nvim-colorizer.lua',
+  opts = {
+    user_default_options = {
+      names = false,
+    },
+  },
+}
